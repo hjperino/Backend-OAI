d23ca1650dea487981eb109772799de700fbb8a2
Hansjuerg Perino <hperino@mac.com>
2025-11-12 18:10:50 +0100

    Update ultimate_api.py

--- ultimate_api.py
+++ ultimate_api.py
@@ -13,8 +13,7 @@ from bs4 import BeautifulSoup
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from traceback import format_exc
-from pydanticsettings import BaseSettings
-from pydantic import BaseModel, ValidationError
+from pydantic import BaseModel, ValidationError, BaseSettings
 from openai import OpenAI
 
 CHUNKSPATH = os.getenv("CHUNKSPATH", "processed/processed_chunks.json")
